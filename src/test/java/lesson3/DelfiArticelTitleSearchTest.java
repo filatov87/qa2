d96ed185bc964dc7be8fa962829677b91c36bd94
Alexey Filatov <alexey.filatov@intexsys.lv>
2018-03-26 15:53:54 +0300

    1-ая домашка с page objects

--- src/test/java/lesson3/DelfiArticelTitleSearchTest.java
+++ src/test/java/lesson3/DelfiArticelTitleSearchTest.java
@@ -6,12 +6,7 @@ import Pages.HomePage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-import java.awt.peer.SystemTrayPeer;
 import java.util.List;
 
 
